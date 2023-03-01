7c48afcd941f121a20c7ef980d06da808918c9ee
GiorgioDellutri <giorgiodll93@gmail.com>
2023-03-01 17:39:46 +0100

    added belongsToMany to model

--- app/Models/Technology.php
+++ app/Models/Technology.php
@@ -5,16 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Post extends Model
+class Technology extends Model
 {
     use HasFactory;
-
-    protected $fillable = ['title', 'slug', 'author', 'content', 'post_date', 'type_id'];
-
-    public function type()
-    {
-        return $this->belongsTo(Type::class);
-    }
 
     public function posts()
     {
